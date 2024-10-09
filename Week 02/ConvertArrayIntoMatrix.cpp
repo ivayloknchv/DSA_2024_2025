28c703beac3449ff7b0da333d497aec21c8d3dba
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-09 21:51:28 +0300

    Update ConvertArrayIntoMatrix.cpp

--- Week 02/ConvertArrayIntoMatrix.cpp
+++ Week 02/ConvertArrayIntoMatrix.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/description/?fbclid=IwZXh0bgNhZW0CMTEAAR0gzMh_YHR0BpMHEfojkn83mKjjKDaBdkAAnz3B4CEb1_9It3Alsq8DBus_aem_-T_-RK2zI0rxH5r-7Do0sA
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) 

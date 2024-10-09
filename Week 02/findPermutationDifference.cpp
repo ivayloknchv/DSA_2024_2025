3882881509701ad404d26872d0941e01af140b15
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-09 10:31:15 +0300

    Update findPermutationDifference.cpp

--- Week 02/findPermutationDifference.cpp
+++ Week 02/findPermutationDifference.cpp
@@ -1,6 +1,34 @@
 //https://leetcode.com/problems/permutation-difference-between-two-strings/description/?fbclid=IwZXh0bgNhZW0CMTEAAR0b8EZjz1lC0NZ9e1AwSoMI6Jpr6wsUSrJWxfa01O5Bs-TDEUrtOv4o28E_aem_5z9ppXTeEKw5scnwfDIMVg
 
+
 class Solution {
+public:
+    int findPermutationDifference(string s, string t)
+     {
+        int indexes[26]{};
+
+        for(int i=0;i<s.size();i++)
+        {
+            indexes[s[i]-'a']=i;           
+        }
+
+        for(int i=0; i<t.size();i++)
+        {
+            indexes[t[i]-'a']-=i;
+        }
+
+        int ans=0;
+
+        for(int i=0;i<26;i++)
+        {
+            ans+=abs(indexes[i]);
+        }
+
+        return ans;
+    }
+};
+
+/*class Solution {
 public:
     int findPermutationDifference(string s, string t)
      {
@@ -22,4 +50,4 @@ public:
 
         return ans;
     }
-};
+}; */

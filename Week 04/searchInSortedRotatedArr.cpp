79f8069188578695bde987f864a8c20fcfb5ac36
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-24 10:19:51 +0300

    Create searchInSortedRotatedArr.cpp

---
+++ Week 04/searchInSortedRotatedArr.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/?envType=problem-list-v2&envId=binary-search
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while(l<=r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[l]<=nums[mid])
+            {
+                if(nums[l]<=target && target<=nums[mid])
+                {
+                    r = mid-1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<=target && target<=nums[r])
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+        }   
+        return -1;
+    }
+};

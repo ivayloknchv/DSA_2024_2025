13e8db4f56b4c3cb3d783058996525a76891c56a
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-25 14:56:01 +0300

    Update minInSortedArr.cpp

--- Week 04/minInSortedArr.cpp
+++ Week 04/minInSortedArr.cpp
@@ -1,36 +1,30 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
-class Solution {
+class Solution 
+{
 public:
     int findMin(vector<int>& nums) 
     {
         int l = 0;
         int r = nums.size() - 1;
-        int ans=nums[0];
+        int ans=INT_MAX;
 
-        while(l<=r)
+        while(l <= r)
         {
             int mid = l + (r - l) / 2;
-
-            if(nums[l]<nums[r])
-            {
-                ans=min(ans, nums[l]);
-                break;
-            }
-
-            ans = min(ans, nums[mid]);
-
+            
             if(nums[l]<=nums[mid])
             {
+                ans=min(ans,nums[l]);
                 l=mid+1;
             }
-
             else
             {
+                ans=min(ans, nums[mid]);
                 r=mid-1;
             }
         }  
 
-        return ans;  
+        return ans;
     }
 };

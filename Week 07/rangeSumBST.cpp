3c143bc911f41f8053e332cd881318b1833646fe
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-11-13 22:44:05 +0200

    Update rangeSumBST.cpp

--- Week 07/rangeSumBST.cpp
+++ Week 07/rangeSumBST.cpp
@@ -33,3 +33,26 @@ public:
         return num + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
     }
 };
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) 
+    {
+        if(!root)
+        {
+            return 0;
+        }
+
+        if(root->val >= low && root->val <= high)
+        {
+            return root->val + rangeSumBST(root->left, low, high) +
+            rangeSumBST(root->right, low, high);
+        }
+        else if(root->val < low)
+        {
+            return rangeSumBST(root->right, low, high);
+        }
+
+        return rangeSumBST(root->left, low, high);
+    }
+};

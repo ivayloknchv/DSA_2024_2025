668c7f48445a1d0f132ac76032d6745f0cc43eba
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-11-21 10:42:23 +0200

    Create fibonacci.cpp

---
+++ Week 08/fibonacci.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/fibonacci-number/description/
+
+class Solution {
+public:
+
+    int fib(int n, vector<int>& cache)
+    {
+        if(n==0)
+        {
+            return cache[0]=0;
+        }
+        
+        if(n==1)
+        {
+            return cache[1]=1;
+        }
+        
+        return cache[n] = fib(n-1, cache) + fib(n-2, cache);
+    }
+    int fib(int n) 
+    {
+        vector<int> fibCache(n+1, 0);
+
+        return fib(n, fibCache); 
+    }
+};

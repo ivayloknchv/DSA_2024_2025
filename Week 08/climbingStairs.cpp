c825d9fb8cc809dede704dc8127e797727e1b1b7
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-11-21 12:10:46 +0200

    Create climbingStairs.cpp

---
+++ Week 08/climbingStairs.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/climbing-stairs/description/
+
+class Solution 
+{
+public:
+    int climbStairs(int n) 
+    {
+        vector<int> steps(n+1, 0);
+        steps[0] = steps[1] = 1;
+
+        for(int i = 2; i <= n; i++)
+        {
+            steps[i]+=steps[i-1];
+            steps[i]+=steps[i-2];
+        }
+
+        return steps[n];
+    }
+};

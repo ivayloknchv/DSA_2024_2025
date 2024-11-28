66a60b90ead1912e044cd3848a84a9ed4d3388c6
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-11-29 00:33:03 +0200

    Create countNicePairs.cpp

---
+++ Week 09/countNicePairs.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/count-nice-pairs-in-an-array/description/?envType=problem-list-v2&envId=hash-table
+
+class Solution {
+public:
+
+    int rev(int num)
+    {
+        int res=0;
+        while(num!=0)
+        {
+            res*=10;
+            res+=num%10;
+            num/=10;
+        }
+
+        return res;
+    }
+
+    int countNicePairs(vector<int>& nums) 
+    {
+        const int mod = 1e9+7;
+        int ans=0;
+        unordered_map<int, int> m;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int curr = (nums[i] - rev(nums[i])) % mod;
+            ans+=m[curr];
+            ans%=mod;
+            m[curr]++;
+            m[curr]%=mod;
+        }
+
+        return ans;
+    }
+};

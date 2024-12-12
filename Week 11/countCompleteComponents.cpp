646c160551f725a7b1106dd91da8f935e918450c
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-12-12 23:59:13 +0200

    Update countCompleteComponents.cpp

--- Week 11/countCompleteComponents.cpp
+++ Week 11/countCompleteComponents.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/count-the-number-of-complete-components/description/
 
-class Solution {
+class Solution1 {
 public:
 
     void dfs(unordered_map<int, unordered_set<int>>& graph, unordered_set<int>& visited,
@@ -61,6 +61,65 @@ public:
                 {
                     ans++;
                 }
+            }    
+        }
+
+        return ans;
+    }
+};
+
+
+class Solution2 {
+public:
+
+    void dfs(unordered_map<int, unordered_set<int>>& graph, unordered_set<int>& visited,
+    int& nodesCount, int&edgesCount , int curr)
+    {
+        visited.insert(curr);
+        nodesCount++;
+        edgesCount+=graph[curr].size();
+
+        for(int neighbour : graph[curr])
+        {
+            if(visited.count(neighbour) == 0)
+            {
+                dfs(graph, visited, nodesCount, edgesCount, neighbour);
+            }
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) 
+    {
+        unordered_map<int, unordered_set<int>> graph;
+
+        for(int i = 0; i < n; i++)
+        {
+            graph[i];
+        }
+
+        for(int i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            graph[u].insert(v);
+            graph[v].insert(u);
+        }
+
+        unordered_set<int> visited;
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+           
+            if(!visited.count(i))
+            {
+                int nodesCount = 0, edgesCount = 0;
+                dfs(graph, visited, nodesCount, edgesCount, i);
+                edgesCount/=2;
+                if(nodesCount*(nodesCount-1)/2 == edgesCount)
+                {
+                    ans++;
+                }
+
             }    
         }
 
